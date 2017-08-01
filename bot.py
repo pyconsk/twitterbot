092542d578380062326ba55f79f1aeea8ffe90ce
PowerFlasher <30596181+PowerFlasher@users.noreply.github.com>
2017-08-01 20:30:27 +0200

    Added command line arguments
    use command bot.py -h for all command line arguments

--- bot.py
+++ bot.py
@@ -27,37 +27,40 @@ def paginate(iterable, page_size):
 
 
 # Users that I'm following
-def existing_followings():
+def existing_followings(pagination, sleep):
     existing_followings = set()
     followers = api.friends_ids()
     print("\nRequesting Existing Followings...\n")
 
-    for page in paginate(followers, 100):
+    for page in paginate(followers, pagination):
         results = api.lookup_users(user_ids=page)
         for result in results:
             # print(result.screen_name) # For Debugging
             existing_followings.add(result.screen_name)
 
-        time.sleep(10)
+        print("Requesting 100 followings....")
+        time.sleep(sleep)
 
     return existing_followings
 
 
 # Users who following me
-def existing_followers():
+def existing_followers(pagination, sleep):
     existing_followers = set()
     followers = api.followers_ids()
     print("\nRequesting Existing Followers...\n")
 
-    for page in paginate(followers, 100):
+    for page in paginate(followers, pagination):
         results = api.lookup_users(user_ids=page)
         for result in results:
             # print(result.screen_name) # For Debugging
             existing_followers.add(result.screen_name)
-
-        time.sleep(10)
+            
+        print("Requesting 100 followers....")
+        time.sleep(sleep)
 
     return existing_followers
+
 
 # Find Users with tweets with searching hashtag
 def users_with_tweets():
@@ -75,36 +78,53 @@ def users_with_tweets():
 
 
 # Make new following
-def follow_user(users_to_follow):
+def follow_user(users_to_follow, sleep):
     print("\nFollowing Users...\n")
     for user in users_to_follow:
         api.create_friendship(screen_name=user)
         print("OK Following User: ", str(user))
-        time.sleep(10)
+        time.sleep(sleep)
+
 
 # Make unfollowing
-def unfollow_user(users_to_unfollow):
+def unfollow_user(users_to_unfollow, sleep):
     print("\nFollowing Users...\n")
     for user in users_to_unfollow:
         api.destroy_friendship(screen_name=user)
         print("OK Unfollowing User: ", str(user))
-        time.sleep(10)
+        time.sleep(sleep)
 
-try:
-    existing_followings = existing_followings()
-    print("\nexisting_followings :", len(existing_followings), "\n", existing_followings)
-    existing_followers = existing_followers()
-    print("\nexisting_followers :", len(existing_followers), "\n", existing_followers)
-    users_with_tweets = users_with_tweets()
-    print("\nusers_with_tweets :", len(users_with_tweets), "\n", users_with_tweets)
-    users_to_follow = (users_with_tweets - (existing_followers.union(existing_followings)))
-    print("\nusers_to_follow :", len(users_to_follow), "\n", users_to_follow)
-    # follow_user(users_to_follow)
-    users_to_unfollow = existing_followings - existing_followers
-    print("\nusers_to_unfollow :", len(users_to_unfollow), "\n", users_to_unfollow)
-    unfollow_user(users_to_unfollow)
-    print("\nDone...")
 
-except tweepy.TweepError as e:
-    e = eval(str(e))
-    print(e[0]['message'], "| Code :", e[0]['code'])
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--action', type=str, required=True, choices=['follow', 'unfollow'],
+                        help='Action - follow | unfollow')
+    parser.add_argument('-p', '--pagination', type=int, default=100, help='Listing pagination')
+    parser.add_argument('-s', '--sleep', type=int, default=10, help='Sleep time for function')
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+    args = parser.parse_args()
+    sleep = args.sleep
+    action = args.action
+    pagination = args.pagination
+    try:
+        existing_followings = existing_followings(pagination, sleep)
+        print("\nexisting_followings :", len(existing_followings), "\n", existing_followings)
+        existing_followers = existing_followers(pagination, sleep)
+        print("\nexisting_followers :", len(existing_followers), "\n", existing_followers)
+        if action == 'follow':
+            users_with_tweets = users_with_tweets()
+            print("\nusers_with_tweets :", len(users_with_tweets), "\n", users_with_tweets)
+            users_to_follow = (users_with_tweets - (existing_followers.union(existing_followings)))
+            print("\nusers_to_follow :", len(users_to_follow), "\n", users_to_follow)
+            follow_user(users_to_follow, sleep)
+        else:
+            users_to_unfollow = existing_followings - existing_followers
+            print("\nusers_to_unfollow :", len(users_to_unfollow), "\n", users_to_unfollow)
+            unfollow_user(users_to_unfollow, sleep)
+        print("\nDone...")
+
+    except tweepy.TweepError as e:
+        e = eval(str(e))
+        print(e[0]['message'], "| Code :", e[0]['code'])
